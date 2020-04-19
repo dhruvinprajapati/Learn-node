c922ac0a02b444afe158d05744c44c11025f5566
dhrvuin <djprajapati1056@outlook.com>
2020-04-19 18:17:45 +0530

    adding route to check path is correct or not

--- hello-node/app.js
+++ hello-node/app.js
@@ -1,10 +1,28 @@
 const http = require('http')
+const fs = require('fs')
 const PORT = 3000
 
 const serve = http.createServer((req,res)=>{
-    res.statusCode = 200
-    res.setHeader('Content_type','text/plain')
-    res.end('hello word')
+    switch(req.url){
+        case '/':
+            fs.readFile('./index.html',(error,data)=>{
+                res.statusCode = 200
+                res.setHeader('Content-Type','text/html')
+                res.end(data)
+            })
+            break
+        case '/hello':
+            fs.readFile('./hello.html',(error,data)=>{
+                res.statusCode = 200
+                res.setHeader('Content-Type','text/html')
+                res.end(data)
+            })
+            break
+        default:
+            res.statusCode = 404
+            res.setHeader('Content-Type','text/html')
+            res.end("Not Found")
+    }
 })
 
 serve.listen(PORT,()=>{
