d1eff71297e3a7fb90f1f31117bffd5493bf0a64
dhrvuin <djprajapati1056@outlook.com>
2020-04-24 23:48:59 +0530

    try mini route with express

--- helloexpress/app.js
+++ helloexpress/app.js
@@ -1,6 +1,10 @@
 const express = require('express');
 const app = express()
 
+app.get('/hello', (req, res) => {
+    res.send("hello Word!");
+});
+
 app.listen(3000, () => {
     console.log(`Server started on port`);
 });
