845947930bc7046ee73b7aaa4c9ccc919a6c0bb7
dhrvuin <djprajapati1056@outlook.com>
2020-04-28 01:11:26 +0530

    use body parser to get data

--- implement-post-route/app.js
+++ implement-post-route/app.js
@@ -1,13 +1,20 @@
 const express = require('express');
+const bodyParser = require('body-parser')
 const app = express()
 const PORT = 3000
+
+//setting up body parsor to parse JSON
+app.use(bodyParser.json())
 
 app.get('/movie', (req, res) => {
     res.send("Movies");
 });
 
 app.post('/movie', (req, res) => {
-    
+    let title = req.body.title
+    let year = req.body.year
+    console.log(title,year)
+    res.send("ok");
 });
 
 app.listen(PORT, () => {
