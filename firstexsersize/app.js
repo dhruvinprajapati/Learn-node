78125eaf5af61f5e2f139a7ce7eced61a52ac86a
dhrvuin <djprajapati1056@outlook.com>
2020-04-28 22:30:28 +0530

    another way appling middleware

--- firstexsersize/app.js
+++ firstexsersize/app.js
@@ -1,16 +1,18 @@
 const express = require('express');
 const app = express()
 const PORT = 3000
+//appling middleware all app route
+app.use(log);
 
 function log(req,res,next){
     console.log("[Aaya]")
     next()
 }
 
-app.get('/',log, (req, res) => {
+app.get('/', (req, res) => {
     res.send("ROOT")
 });
-app.get('/login',log, (req, res) => {
+app.get('/login', (req, res) => {
     res.send("login")
 });
 
