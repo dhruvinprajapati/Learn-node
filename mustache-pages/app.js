58c5bfe2723b733326097713fc39fe8de8a1af57
dhrvuin <djprajapati1056@outlook.com>
2020-04-28 14:10:18 +0530

    on form click take data server side

--- mustache-pages/app.js
+++ mustache-pages/app.js
@@ -1,11 +1,25 @@
 const express = require('express');
-const app = express()
 const mustacheExpress = require('mustache-express')
+const bodyParser = require('body-parser')
+const app = express()
 const PORT = 3000
+
 
 app.engine('mustache',mustacheExpress())
 app.set('views', './views');
 app.set('view engine', 'mustache');
+app.use(bodyParser.urlencoded({extended:false}));
+
+app.get('/add-user', (req, res) => {
+    res.render('add-user');
+});
+
+app.post('/add-user', (req, res) => {
+    let name = req.body.name
+    let age = req.body.age
+    console.log(name,age)
+    res.status(200).send("ok");
+});
 
 app.get('/users', (req, res) => {
     let users = [
