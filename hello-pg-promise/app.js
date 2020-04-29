c8f3e723fa6dbdb7f02216b3a752a751ff5acf1d
dhrvuin <djprajapati1056@outlook.com>
2020-04-29 18:49:32 +0530

    insert data from database

--- hello-pg-promise/app.js
+++ hello-pg-promise/app.js
@@ -1,5 +1,18 @@
 const pgp = require('pg-promise')()
-const connectionString = "postgres://localhost:5432/nailasgarden"
+const connectionString = "postgres://localhost:5432/nailasgraden"
 const db = pgp(connectionString)
 
-console.log(db)
+// console.log(db)
+//.none is not return anything
+/*db.none('INSERT INTO dishes(name,course,price,imageurl)VALUES($1,$2,$3,$4)',
+['kishor','entres',5.9,'hoho'])
+.then(()=>{
+    console.log("SUCCESS")
+}).catch(error => console.log(error))*/
+
+//return id 
+db.one('INSERT INTO dishes(name,course,price,imageurl)VALUES($1,$2,$3,$4)RETURNING dishid',
+['hot','staters',1.9,'hehe'])
+.then((data)=>{
+    console.log(data)
+}).catch(error => console.log(error))
